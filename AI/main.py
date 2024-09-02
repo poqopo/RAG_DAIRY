51e7ef1c07bd58cdb9c1907d82451a12d6c09186
poqopo <block.poqopo@gmail.com>
2024-09-02 17:17:37 +0900

    Using RAG

--- AI/main.py
+++ AI/main.py
@@ -3,6 +3,7 @@ import faiss
 from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
 
 load_dotenv()
 
@@ -13,9 +14,26 @@ embeddings = OpenAIEmbeddings()
 dimension_size = len(embeddings.embed_query("hello world"))
 print(dimension_size)
 
-db = FAISS(
-    embedding_function=OpenAIEmbeddings(),
-    index=faiss.IndexFlatL2(dimension_size),
-    docstore=InMemoryDocstore(),
-    index_to_docstore_id={},
+db = FAISS.from_texts(
+    ["안녕하세요. 정말 반갑습니다.", "제 이름은 테디입니다."],
+    embedding=OpenAIEmbeddings(),
+    metadatas=[{"source": "user1"}, {"source": "user1"}],
+    ids=["user1_2024.08.28", "user1_2024.08.29"],
 )
+print(db.index_to_docstore_id)
+print(db.docstore._dict)
+
+db.add_texts(
+    ["이번엔 텍스트 데이터를 추가합니다."],
+    metadatas=[{"source": "user2"}],
+    ids=["user2_2024.08.29"],
+)
+
+db.save_local(folder_path="faiss_db", index_name="faiss_index")
+
+# loaded_db = FAISS.load_local(
+#     folder_path="faiss_db",
+#     index_name="faiss_index",
+#     embeddings=embeddings,
+#     allow_dangerous_deserialization=True,
+# )
